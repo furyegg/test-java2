2ae38a75ffde76d7e4562d5bd9db3872daa9a5db
Kyle Li <Kyle.Li@lombardrisk.com>
2016-03-22 09:55:20 +0800

    test

--- test/src/main/java/Test.java
+++ test/src/main/java/Test.java
@@ -1,20 +1,14 @@
-import com.google.common.collect.HashMultimap;
-import com.google.common.collect.Lists;
+import com.google.common.base.Function;
+import com.google.common.collect.FluentIterable;
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Maps;
-import com.google.common.collect.Multimap;
-import com.google.common.collect.Sets;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.math.BigDecimal;
-import java.math.MathContext;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Date;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 public class Test {
 
@@ -26,45 +20,16 @@ public class Test {
 
 	// System.out.println(list);
 	public static void main(String[] args) throws IOException {
-		Test t = new Test();
-		List<String> list = arrayListIfNull(t.getList());
-//		List<String> list = t.getList();
-//		if (list == null) {
-//			list = new ArrayList<>();
-//		}
-		list.add("a");
-		System.out.println(t.getList());
+		Map<Integer, String> map = Maps.newTreeMap();
+		map.put(1, "a");
+		map.put(3, "b");
+		map.put(4, "c");
+		map.put(2, "d");
+		map.values().forEach(System.out::println);
 	}
 
-	public static <T> Set<T> hashSetIfNull(Set<T> set) {
-		if (set == null) {
-			set = Sets.newHashSet();
-		}
-		return set;
+	public static void test(Object... params) {
+		System.out.println(params.length);
 	}
 
-	public static <T extends Comparable> Set<T> treeSetIfNull(Set<T> set) {
-		if (set == null) {
-			set = Sets.newTreeSet();
-		}
-		return set;
-	}
-
-	public static <T> List<T> arrayListIfNull(List<T> list) {
-		if (list == null) {
-			list = Lists.newArrayList();
-		}
-		return list;
-	}
-
-	public static <T> List<T> linkedListIfNull(List<T> list) {
-		if (list == null) {
-			list = Lists.newLinkedList();
-		}
-		return list;
-	}
-
-	public List<String> getList() {
-		return list;
-	}
 }
