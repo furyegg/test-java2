daf3f17bfd1c56bbc5fb826135fe4a289edfcacd
lyhtbc <lyhtbc@gmail.com>
2016-11-07 08:53:57 +0800

    test

---
+++ test/src/main/java/variance/VarianceTest.java
@@ -0,0 +1,14 @@
+package variance;
+
+/**
+ * Created by Administrator on 2016/7/29.
+ */
+public class VarianceTest {
+	public static void main(String[] args) {
+		A a = new A();
+		B b = new B();
+
+		Foo foo = new Foo();
+		System.out.println(foo.test(a));
+	}
+}
