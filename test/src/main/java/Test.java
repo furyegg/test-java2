f4f2ba768d0296be6e5f742287a917816797f43e
za-liyiheng <liyiheng@zhongan.io>
2019-01-28 12:24:44 +0800

    test

--- test/src/main/java/Test.java
+++ test/src/main/java/Test.java
@@ -1,28 +1,16 @@
 import com.google.common.base.Joiner;
 import com.google.common.collect.Lists;
+import io.vavr.collection.Stream;
 import one.util.streamex.StreamEx;
 
+import java.util.List;
 import java.util.concurrent.ExecutionException;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class Test {
     
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-//        Pattern p = Pattern.compile("cat");
-//        Matcher m = p.matcher("one cat two cats in the yard");
-//        StringBuffer sb = new StringBuffer();
-//        while (m.find()) {
-//            m.appendReplacement(sb, "dog");
-//        }
-//        m.appendTail(sb);
-//        System.out.println(sb.toString());
-    
-        Pattern p = Pattern.compile("cat");
-        Matcher m = p.matcher("one cat two cats in the yard");
-        while (m.find()) {
-            System.out.println(m.group());
-        }
+        System.out.println(String.format("%1$d, %1$d", 5));
     }
-
+    
 }
